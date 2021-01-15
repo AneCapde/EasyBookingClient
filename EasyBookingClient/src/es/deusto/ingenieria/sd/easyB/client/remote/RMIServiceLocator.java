dc3fa14dd6fbf0686061f2f9f58c9af4e397611e
olatzinfante <47973617+olatzinfante@users.noreply.github.com>
2021-01-15 14:54:30 +0100

    ServiceLocator

--- EasyBookingClient/src/es/deusto/ingenieria/sd/easyB/client/remote/RMIServiceLocator.java
+++ EasyBookingClient/src/es/deusto/ingenieria/sd/easyB/client/remote/RMIServiceLocator.java
@@ -1,6 +1,12 @@
 package es.deusto.ingenieria.sd.easyB.client.remote;
 
+import java.rmi.Naming;
+
+import es.deusto.ingenieria.sd.easyB.server.remote.IEasyBookingRemoteFacade;
+
 public class RMIServiceLocator {
+	
+	private IEasyBookingRemoteFacade service;
 
 	public RMIServiceLocator() {     
     	if (System.getSecurityManager() == null) {
@@ -8,12 +14,17 @@ public class RMIServiceLocator {
     	}
     }
 	
-	   public void setService(String ip, String port, String serviceName) { 
-	    	try {
-	    		String name = "//" + ip + ":" + port + "/" + serviceName;
-	    		//
-	    	} catch (Exception ex) {
-	    		System.err.println("#ERROR looking up for the remote service: " + ex);
-	    	}
-	    }
+   public void setService(String ip, String port, String serviceName) { 
+    	try {
+    		String name = "//" + ip + ":" + port + "/" + serviceName;
+    		this.service = (IEasyBookingRemoteFacade) Naming.lookup(name);
+     	} catch (Exception ex) {
+    		System.err.println("#ERROR looking up for the remote service: " + ex);
+    	}
+    }
+   
+   public IEasyBookingRemoteFacade getService() {
+	   return this.service;
+   }
+   
 }
