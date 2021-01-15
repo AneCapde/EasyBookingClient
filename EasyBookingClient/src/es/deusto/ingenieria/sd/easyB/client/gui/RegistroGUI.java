ef4a7b67f62a73abc7e5d99b58b11f7bacc79852
olatzinfante <47973617+olatzinfante@users.noreply.github.com>
2021-01-15 15:20:27 +0100

    RegistroGUI

---
+++ EasyBookingClient/src/es/deusto/ingenieria/sd/easyB/client/gui/RegistroGUI.java
@@ -0,0 +1,51 @@
+package es.deusto.ingenieria.sd.easyB.client.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class RegistroGUI {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RegistroGUI window = new RegistroGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RegistroGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(329, 168, 96, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+	}
+}
