0bc3db529e56b00958bfe4d23d4ca2c8ffea7bdb
olatzinfante <47973617+olatzinfante@users.noreply.github.com>
2021-01-15 18:15:22 +0100

    RegistroGUI

--- EasyBookingClient/src/es/deusto/ingenieria/sd/easyB/client/gui/RegistroGUI.java
+++ EasyBookingClient/src/es/deusto/ingenieria/sd/easyB/client/gui/RegistroGUI.java
@@ -2,13 +2,22 @@ package es.deusto.ingenieria.sd.easyB.client.gui;
 
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.JPasswordField;
 
 public class RegistroGUI {
 
-	private JFrame frame;
+	private JFrame frmRegistro;
 	private JTextField textField;
+	private JPasswordField passwordField;
+	private ButtonGroup btnGroupSistAut = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -18,7 +27,7 @@ public class RegistroGUI {
 			public void run() {
 				try {
 					RegistroGUI window = new RegistroGUI();
-					window.frame.setVisible(true);
+					window.frmRegistro.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -37,15 +46,47 @@ public class RegistroGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmRegistro = new JFrame();
+		frmRegistro.setTitle("Registro");
+		frmRegistro.setResizable(false);
+		frmRegistro.setBounds(100, 100, 800, 600);
+		frmRegistro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmRegistro.getContentPane().setLayout(null);
+		
+		JLabel lblReg = new JLabel("Registrarse usando...");
+		lblReg.setBounds(175, 100, 200, 15);
+		frmRegistro.getContentPane().add(lblReg);
+		
+		JRadioButton rdbtnGoogle = new JRadioButton("Google");
+		rdbtnGoogle.setBounds(175, 134, 103, 21);
+		frmRegistro.getContentPane().add(rdbtnGoogle);
+		rdbtnGoogle.setSelected(true);
+		btnGroupSistAut.add(rdbtnGoogle);
+		
+		JRadioButton rdbtnFacebook = new JRadioButton("Facebook");
+		rdbtnFacebook.setBounds(175, 169, 103, 21);
+		frmRegistro.getContentPane().add(rdbtnFacebook);
+		btnGroupSistAut.add(rdbtnFacebook);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(117, 207, 564, 2);
+		frmRegistro.getContentPane().add(separator);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(69, 252, 96, 13);
+		frmRegistro.getContentPane().add(lblEmail);
 		
 		textField = new JTextField();
-		textField.setBounds(329, 168, 96, 19);
-		frame.getContentPane().add(textField);
+		textField.setBounds(175, 249, 228, 19);
+		frmRegistro.getContentPane().add(textField);
 		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(175, 306, 228, 19);
+		frmRegistro.getContentPane().add(passwordField);
+		
+		JLabel lblPass = new JLabel("Contrase\u00F1a:");
+		lblPass.setBounds(69, 309, 84, 13);
+		frmRegistro.getContentPane().add(lblPass);
 	}
 }
