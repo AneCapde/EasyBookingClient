5e692054f230cd5d600b7b559ef66c72c9997572
olatzinfante <47973617+olatzinfante@users.noreply.github.com>
2021-01-17 15:42:32 +0100

    AutorizacionController

--- EasyBookingClient/src/es/deusto/ingenieria/sd/easyB/client/controller/AutorizacionController.java
+++ EasyBookingClient/src/es/deusto/ingenieria/sd/easyB/client/controller/AutorizacionController.java
@@ -1,9 +1,25 @@
 package es.deusto.ingenieria.sd.easyB.client.controller;
 
+import java.rmi.RemoteException;
+
 import es.deusto.ingenieria.sd.easyB.client.remote.RMIServiceLocator;
 
 public class AutorizacionController {
 	
 	public RMIServiceLocator serviceLocator;
+	
+	public AutorizacionController(RMIServiceLocator serviceLocator) {
+		this.serviceLocator = serviceLocator;
+	}
+	
+	/*public boolean login(String email, String password) {
+		try {
+			this.serviceLocator.getService().login(email, password);
+			
+		} catch (RemoteException e) {
+			System.err.println("# Error durante el LogIn: " + e);
+			return false;
+		}
+	}*/
 
 }
